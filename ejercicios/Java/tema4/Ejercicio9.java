584491e51fa37a2812ebc44fbdfbb83e9e1d2557
Ángeles Bueno Aguilar <angelesbueno86@gmail.com>
2017-10-19 00:14:08 +0200

    Añadido ejercicio 9 incompleto

---
+++ ejercicios/Java/tema4/Ejercicio9.java
@@ -0,0 +1,28 @@
+/**
+ * 4. Sentencia condicional
+ *
+ * 8. Realiza un programa que resuelva una ecuación de segundo grado 
+ *    (del tipo ax2 +bx+c = 0).
+ *
+ * @author Ángeles Bueno
+ */
+
+
+ public class Ejercicio9 {
+  public static void main(String[] args) {
+    
+    System.out.println("Vamos a resolver una ecuación de segundo grado del tipo ax^2 +bx+c = 0");
+    System.out.print("Introduce un valor para a: ");
+      double a = Double.parseDouble(System.console().readLine());
+    System.out.print("Introduce un valor para b: ");
+      double b = Double.parseDouble(System.console().readLine());
+    System.out.print("Introduce un valor para c: ");
+      double c = Double.parseDouble(System.console().readLine());
+      
+      
+  }
+}
+
+//corregir
+      
+    
