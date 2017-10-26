45a413212926dee2a54874877047ac2c9be5ce7f
Ángeles Bueno Aguilar <angelesbueno86@gmail.com>
2017-10-27 00:05:49 +0200

    Añadido ejercicio4

---
+++ ejercicios/Java/tema5/Ejercicio4.java
@@ -0,0 +1,18 @@
+/**
+ * 5. Bucles.
+ *
+ * 4. Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando un bucle for.
+ *
+ * @author Ángeles Bueno
+ */
+ 
+public class Ejercicio4 {
+  public static void main(String[] args) {
+    
+    for (int i = 320; ((i <= 320) && (i >= 160)); i-=20) {
+      System.out.println(i);
+    }
+  }
+}
+    
+    
