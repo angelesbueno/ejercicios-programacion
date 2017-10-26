9cda298101bffc56efe15090a6b15bbe714b846d
Ángeles Bueno Aguilar <angelesbueno86@gmail.com>
2017-10-27 00:42:16 +0200

    Añadido ejercicio8

---
+++ ejercicios/Java/tema5/Ejercicio8.java
@@ -0,0 +1,16 @@
+/**
+ * 5. Bucles.
+ *
+ * 8. Muestra la tabla de multiplicar de un número introducido por teclado.
+ *
+ * @author Ángeles Bueno
+ */
+ 
+public class Ejercicio8 {
+  public static void main(String[] args) {
+    
+    for (int num = 0; num < 11; num++) {
+      System.out.println("2 x " + num + " = " + (2 * num));
+    }
+  }
+}
