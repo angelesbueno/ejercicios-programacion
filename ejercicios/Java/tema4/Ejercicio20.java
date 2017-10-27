30d36977b833228c3b03bd1f3ff28df100c9dc99
Ángeles Bueno Aguilar <angelesbueno86@gmail.com>
2017-10-27 23:15:04 +0200

    Subido ejercicio20 incompleto

---
+++ ejercicios/Java/tema4/Ejercicio20.java
@@ -0,0 +1,22 @@
+/**
+ * 4. Sentencia condicional
+ *
+ * 20. Realiza un programa que diga si un número entero positivo introducido 
+ *     por teclado es capicúa. Se permiten números de hasta 5 cifras.
+ *
+ * @author Ángeles Bueno
+ */
+ 
+ 
+public class Ejercicio20 {
+  public static void main(String[] args) {
+    
+    System.out.print("Introduce un número positivo y de hasta 5 cifras y te diré si es capicúa: ");
+    int numero = Integer.parseInt(System.console().readLine());
+    
+    if ((numero 
+    
+    
+    incompleto
+    
+    
