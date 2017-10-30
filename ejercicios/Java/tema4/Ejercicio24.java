53aaef2a84bf8c0898fc9d9f109d6db878424bee
Ángeles Bueno Aguilar <angelesbueno86@gmail.com>
2017-10-30 23:31:46 +0100

    Añadido ejercicio10

---
+++ ejercicios/Java/tema4/Ejercicio24.java
@@ -0,0 +1,18 @@
+/**
+ * 4. Sentencia condicional
+ *
+ * 24. Escribe un programa que calcule el precio final de un producto según 
+       su base imponible (precio antes de impuestos), el tipo de IVA aplicado 
+       (general, reducido o superreducido) y el código promocional. 
+       Los tipos de IVA general, reducido y superreducido son del 21%, 10% y 
+       4% respectivamente. Los códigos promocionales pueden ser nopro, mitad, 
+       meno5 o 5porc que significan respectivamente que no se aplica promoción, 
+       el precio se reduce a la mitad, se descuentan 5 euros o se descuenta el 5%. 
+       El ejercicio se da por bueno si se muestran los valores correctos, aunque 
+       los números no estén tabulados.
+ *
+ * @author Ángeles Bueno
+ */
+ 
+public class Ejercicio24 {
+  public static void main(String[] args) {
--- ejercicios/Java/tema5/Ejercicio10.java
+++ ejercicios/Java/tema5/Ejercicio10.java
@@ -1,10 +1,47 @@
 /**
  * 5. Bucles.
  *
- * 10. Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle do-while.
+ * 10. Escribe un programa que calcule la media de un conjunto de números positivos 
+*      introducidos por teclado. A priori, el programa no sabe cuántos números 
+*      se introducirán. El usuario indicará que ha terminado de introducir 
+*      los datos cuando meta un número negativo.
  *
  * @author Ángeles Bueno
  */
  
 public class Ejercicio10 {
   public static void main(String[] args) {
+    
+    int contador = 0;
+    double suma = 0;
+  
+    System.out.println("Vamos a calcular la media de un conjunto de números positivos, te mostraré el resultado cuando introduzcas un número negativo");
+    System.out.println("");
+    System.out.print("Introduce un número positivo: ");
+    double num = Double.parseDouble(System.console().readLine());
+    
+    
+    if ((num < 0) && (contador == 0)) {
+      System.out.print("Introduce al menos un número positivo");
+    }
+    
+      
+    while (num >= 0) {
+      System.out.print("Introduce un número positivo: ");
+      num = Integer.parseInt(System.console().readLine());
+      contador++;
+      suma += num;
+    }
+    
+    if ((num < 0) && (contador > 1)) {
+      System.out.print("La media de todos los números positivos es de: " + (suma - num) / (contador - 1));
+    }
+  }
+}
+      
+      
+        
+    
+    
+      
+      
