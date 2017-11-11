9dce5b1023446639c3f454356fac430e3f0ed937
Ángeles Bueno Aguilar <angelesbueno86@gmail.com>
2017-11-11 13:48:29 +0100

    Añadidos ejercicios examen DAM

--- ejercicios/Java/tema5/Ejercicio17.java
+++ ejercicios/Java/tema5/Ejercicio17.java
@@ -7,6 +7,42 @@
  *
  * @author Ángeles Bueno
  */
- 
+//sumar todos los números entre dos números introducidos por teclado
 public class Ejercicio17 {
   public static void main(String[] args) {
+    
+    System.out.print("Introduce el primer número: ");
+    int num1 = Integer.parseInt(System.console().readLine());
+    
+    System.out.print("Introduce el segundo número: ");
+    int num2 = Integer.parseInt(System.console().readLine());
+    
+    int i = num1;
+    
+    int suma = 0;
+    
+    while (i < num2) {   //i = 3    num2 = 5    suma = 2
+    
+      
+      suma += i;
+      i++;
+      
+    }
+    
+    System.out.print("El valor total e la suma es :" + suma);
+    
+  }
+}
+    
+    
+    
+    
+    
+      
+    
+    
+    
+
+
+
+
