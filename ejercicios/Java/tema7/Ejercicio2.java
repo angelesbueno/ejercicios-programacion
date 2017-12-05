2cde2db77bbca4859567f151a6cd114fa88ccfb7
Ángeles Bueno Aguilar <angelesbueno86@gmail.com>
2017-12-05 08:46:36 +0100

    Añadidos ejercicios

---
+++ ejercicios/Java/tema7/Ejercicio2.java
@@ -0,0 +1,13 @@
+/**
+ * 7. Arrays.
+ *
+ * 2. Define un array de 10 caracteres con nombre simbolo y asigna valores a los
+      elementos según la tabla que se muestra a continuación. Muestra el contenido
+      de todos los elementos del array. ¿Qué sucede con los valores de los elementos
+      que no han sido inicializados?
+ *
+ * @author Ángeles Bueno
+ */
+ 
+public class Ejercicio2 {
+  public static void main(String[] args) {
