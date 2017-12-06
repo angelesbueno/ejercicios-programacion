5266911f728adb9b4974a4ae56bd11c706488932
Ángeles Bueno Aguilar <angelesbueno86@gmail.com>
2017-12-06 20:48:05 +0100

    Actualizado ejercicios

--- ejercicios/Java/tema7/Ejercicio12.java
+++ ejercicios/Java/tema7/Ejercicio12.java
@@ -10,13 +10,13 @@ public class Ejercicio12 {
   public static void main(String[] args) {
     
     int [] numero = new int[10];
-    int contadorPrimo = 0;
-    int contadorNoPrimo = 0;
+    int [] auxiliar = new int[10];
     
     for (int i = 0; i < 10; i++) {
       
       System.out.print("Introduzca un número: ");
       numero[i] = Integer.parseInt(System.console().readLine());
+      auxiliar[i] = numero[i];
     }
     
     System.out.println();  
@@ -35,15 +35,51 @@ public class Ejercicio12 {
       
       System.out.print(numero[k] + "    ");
     }
-    
+    System.out.println();
+    System.out.println();
     System.out.print("Indique la posición inicial: ");
     int inicial = Integer.parseInt(System.console().readLine()); 
     System.out.print("Indique la posición final: ");
     int ultima = Integer.parseInt(System.console().readLine()); 
     System.out.println();
     
-    if ((inicial < ultima) && (inicial >= 0 && ultima >= 0)) {
+    //error de rango de array desde aquí
+    
+    if ((inicial < ultima) && ((inicial >= 0 && inicial <= 9) && (ultima >= 0 && ultima <= 9))) {
+      
+      for (int i = 0; i <= 9; i++) {
+
+        numero[ultima] = auxiliar[inicial];
+        numero[ultima + (i + 1)] = auxiliar[ultima];
+        numero[ultima + (i + 2)] = auxiliar[ultima + (i + 1)];
+        
+        if (i == 9) {
+            
+          numero[0] = auxiliar[9];
+        }
+      }
     }
+        
+    
+    System.out.println();  
+    System.out.println("Serie nueva: ");
+    System.out.println();
+    System.out.print("Índice    ");
+      
+    for (int j = 0; j <= 10; j++) {
+        
+      System.out.print(j + "    ");
+    }
+    System.out.println();
+    System.out.print("Valor     ");
+    System.out.println();
+    
+    for (int k = 0; k <= 10; k++) {
+      
+      System.out.print(numero[k] + "    ");
+    }
+          
+    
     
   }
 }
