b898de44ae4f6a6a6bfbf03c1995703ec9364565
Ángeles Bueno Aguilar <angelesbueno86@gmail.com>
2017-12-06 16:38:15 +0100

    Añadido ejercicio

--- ejercicios/Java/tema6/Ejercicio24.java
+++ ejercicios/Java/tema6/Ejercicio24.java
@@ -11,19 +11,41 @@ public class Ejercicio24 {
   public static void main(String[] args) {
     
     System.out.print("Por favor, introduzca un número entero positivo: ");
-    int numeroIntroducido = Integer.parseInt(System.console().readLine());
-    int digito = 0;
+    long numeroIntroducido = Integer.parseInt(System.console().readLine());
+    long auxiliar = numeroIntroducido;
+    long digito = 0;
+    int contadorDigitos = 0;
     
     while (numeroIntroducido >= 1) {
         
       digito = (digito * 10) + (numeroIntroducido % 10);
-        
+      contadorDigitos ++;
       numeroIntroducido /= 10;
     }
     
-    System.out.println(digito);
-    System.out.println(numeroIntroducido);
-    //incompleto
+    System.out.println(contadorDigitos);
+    
+    long [] numero = new long[contadorDigitos];
+    int j = 0;
+    
+    while (auxiliar >= 1) {
+        
+      numero[j] = auxiliar % 10;
+      auxiliar /= 10;
+      j ++;
+    }
+    
+    long numAzar = ((long)(Math.random() * contadorDigitos));
+    
+    System.out.println(numAzar);
+    
+    //mal, preguntar.
+    
+    
+    /**for (int i = 0; i < contadorDigitos; i ++) {
+        
+      System.out.print(numero[i] + " ");*/
+    
   }
 }
     
