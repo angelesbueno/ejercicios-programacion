ba650d083f932483bded38012ddd410d1c52a9c1
Ángeles Bueno Aguilar <angelesbueno86@gmail.com>
2018-01-15 23:25:16 +0100

    Añadidos ejercicios tema 8

---
+++ ejercicios/Java/tema8/Ejercicio17.java
@@ -0,0 +1,12 @@
+/**
+ * Tema 8. Funciones
+ * Ejercicio 17.
+ * Escribe un programa que pase de binario a decimal.
+ * 
+ * @author Ángeles Bueno
+ */
+ 
+import matematicas.Funciones;
+
+public class Ejercicio17 {
+  public static void main(String[] args) {
