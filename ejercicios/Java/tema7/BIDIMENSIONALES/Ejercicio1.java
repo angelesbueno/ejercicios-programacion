6b69610bce5caf9a9454fcb996536574e72bf7bd
Ángeles Bueno Aguilar <angelesbueno86@gmail.com>
2018-01-19 09:00:58 +0100

    Añadidos Arrays Bidimensionales

---
+++ ejercicios/Java/tema7/BIDIMENSIONALES/Ejercicio1.java
@@ -0,0 +1,41 @@
+/**
+ * 7. Arrays.
+ *
+ * 1. Define un array de números enteros de 3 filas por 6 columnas con nombre num
+y asigna los valores según la siguiente tabla. Muestra el contenido de todos
+los elementos del array dispuestos en forma de tabla como se muestra en la
+figura.
+ *
+ * @author Ángeles Bueno
+ */
+ 
+public class Ejercicio1 {
+  public static void main(String[] args) {
+    
+    int [][] numero = new int[3][6];
+    
+    numero [0][1] = 30;
+    numero [0][2] = 2;
+    numero [0][5] = 5;
+    numero [1][0] = 75;
+    numero [2][2] = -2;
+    numero [2][3] = 9;
+    numero [2][5] = 11;
+    
+    int fila = 0;
+    int columna = 0;
+    
+    System.out.println("Array num  Columna 0  Columna 1  Columna 2  Columna 3  Columna 4  Columna 5");
+    
+    for (fila = 0; fila < 3; fila ++) {
+      System.out.print("Fila " + fila);
+      for (columna = 0; columna < 6; columna ++) {
+        System.out.printf("%10d ", numero[fila][columna]);
+      }
+      System.out.println();
+    }
+  }
+}
+         
+      
+    
--- ejercicios/Java/tema8/Prueba.java
+++ ejercicios/Java/tema8/Prueba.java
@@ -10,12 +10,33 @@ public class Prueba {
   public static void main(String[] args) {
     
     System.out.print("Introduce un número: ");
-    int numeroIntroducido = Integer.parseInt(System.console().readLine());
+    int numero = Integer.parseInt(System.console().readLine());
+    System.out.print("Dime un dígito: ");
+    int digito = Integer.parseInt(System.console().readLine());
+    int posicion = -1;
+    int noEncuentra = 0;
+    int volteado = matematicas.Funciones.voltea(numero);
+    System.out.print(volteado);
+    while (volteado > 0) {
+      if (volteado % 10 == digito) {
+        volteado = 0;
+        posicion ++;
+      } else {
+      noEncuentra ++;
+      }
+      volteado /= 10;
+    }
     
-    if (matematicas.Funciones.esCapicua(numeroIntroducido)) {
-      System.out.println("El número " + numeroIntroducido + " es capicúa.");
+    if (noEncuentra == 0) {
+      System.out.print(-1);
     } else {
-      System.out.println("El número " + numeroIntroducido + " no es capicúa.");
+    System.out.println(posicion);
     }
   }
 }
+  
+    
+      
+      
+    
+    
